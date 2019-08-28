1c9002f55bcb79d9503875cd8318bbd7d2f7d1b5
MaxBrohman <maksimbrochman@gmail.com>
2019-08-28 11:42:38 +0300

    first build version added

---
+++ docs/precache-manifest.99cf6fb87126590457f62aaa5a69886e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbd5909dd2bd6c0dc3bc9799aca3b30",
+    "url": "/map-pointer/index.html"
+  },
+  {
+    "revision": "924a5e0468ff0e67edc8",
+    "url": "/map-pointer/static/css/main.5cac9865.chunk.css"
+  },
+  {
+    "revision": "8392bf6c8c11453d9903",
+    "url": "/map-pointer/static/js/2.3c3226e2.chunk.js"
+  },
+  {
+    "revision": "924a5e0468ff0e67edc8",
+    "url": "/map-pointer/static/js/main.fc4978a5.chunk.js"
+  },
+  {
+    "revision": "533fcecb63b4f3e852b3",
+    "url": "/map-pointer/static/js/runtime~main.ad907a0f.js"
+  }
+]);
